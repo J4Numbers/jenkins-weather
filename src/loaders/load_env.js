fc60149f1ce90694702aed4ee7d9157a942df5d2
J4Numbers <j4numbers@gmail.com>
2019-02-13 22:00:21 +0000

    fix(dotenv): sorted out some missing details in the dotenv usage

--- src/loaders/load_env.js
+++ src/loaders/load_env.js
@@ -1,4 +1,1 @@
-import dotenv from 'dotenv';
-
-console.log(`'${process.env.NODE_ENV.toLowerCase() || 'dev'}'`);
-dotenv.config({ path: `../config/${(process.env.NODE_ENV).toLowerCase() || 'dev'}.env` });
+console.log(process.env);
