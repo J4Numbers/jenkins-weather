4397362a1c816d2e3fdd205171971b3f85c15dd5
J4Numbers <j4numbers@gmail.com>
2019-02-14 21:16:26 +0000

    feat(jenkins): did a vast transformation to move into using axios
    We are now using axios instead of the jenkins api since it doesn't seem
    to play too nicely with client-side JS applications. Also brought in a
    docker compose for a re-deployable Jenkins instance wherever we go.
    Might want to add in some general configurations into it so that it can
    be spun up all at once.

--- src/loaders/jenkins_loader.js
+++ src/loaders/jenkins_loader.js
@@ -1,12 +1,17 @@
 import jenkins from 'jenkins';
 
-export default (config) => {
-  const jenkinsMgr = jenkins({ baseUrl: config.jenkins_url, crumbIssuer: config.crumb_enabled, headers: { 'Access-Control-Allow-Origin': '*' } });
-  jenkinsMgr.info((err, data) => {
-    if (err) {
-      console.error(err);
-    }
-    console.log(data);
-  });
-  return jenkinsMgr;
+let jenkinsManager = undefined;
+
+export default (config = {}) => {
+  if (jenkinsManager === undefined) {
+    jenkinsManager = jenkins(
+      {
+        promisify: true,
+        baseUrl: config.jenkins_url,
+        headers: {
+          'Authorization': 'Basic user:password'
+        }
+      });
+  }
+  return jenkinsManager;
 };
